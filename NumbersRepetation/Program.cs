0fa048d03ebe8149f5d494670055f20f0935f965
karunakargoud <karunakargoud1116@gmail.com>
2024-11-19 11:20:51 +0530

    FirstCommit

---
+++ NumbersRepetation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersRepetation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sequence of numbers:");
+            int[] arr = { 1, 2, 3, 2, 3, 1, 3 };
+
+            Dictionary<int,int> dic = new Dictionary<int,int>();
+            foreach(var k in arr)
+            {
+                if (dic.ContainsKey(k))
+                {
+                    dic[k]++;
+                }
+                else
+                {
+                    dic[k] = 1;
+                }
+            }
+            foreach (var k in dic)
+            {
+                Console.WriteLine(k.Key +"-"+k.Value);
+            }
+        }
+    }
+}
